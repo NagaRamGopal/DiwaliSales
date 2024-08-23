fab173c0d08db5c79969a0e9323d797201531fc9
Ram <nagaram.annavarapu@gmail.com>
2024-08-23 09:58:43 -0400

    Done with finding most purchased age group with gender

--- EDA.py
+++ EDA.py
@@ -3,11 +3,12 @@ import pandas_profiling as pp
 from pandas_profiling import ProfileReport
 
 import matplotlib.pyplot as plt
+import seaborn as sb
 
 df=pd.read_csv(r'C:\Users\ramgo\OneDrive\Desktop\Learn\DiwaliSalesAnalysis\DiwaliSales\CleanedData.csv')
 
-rpt=ProfileReport(df, title="Final Data")
-rpt.to_file("FinalData.html")
+#rpt=ProfileReport(df, title="Final Data")
+#rpt.to_file("FinalData.html")
 
 amt_by_gender=df.groupby('Gender')['Amount'].sum()
 print(amt_by_gender)
@@ -21,5 +22,22 @@ plt.show()
 
 avg_amt=df.groupby('Gender')['Amount'].mean()
 print(avg_amt.astype(int))
-#This gives the average amount spent by male and female.
+#This gives the average amount spent by male and female. Here we can even though pp of females>males, male's avg>female's avg.
 
+age_spent_amount=df.groupby('Age Group')['Amount'].sum()
+age_spent_amount.plot(kind='bar')
+plt.xlabel('Age Group')
+plt.ylabel('Amount')
+plt.show()
+#Throught this we can see 26-35 spends more than other age groups.
+
+
+sb.countplot(df, x='Age Group', hue='Gender')
+#This gives the Age Group spending amount divided by gender.
+
+tv=sb.countplot(df, x='Age Group', hue='Gender')
+for bars in tv.containers:
+  tv.bar_label(bars)
+#This gives the same but also includes values
+
+#This conlcudes most of the buyers are females from 26-35 age.
